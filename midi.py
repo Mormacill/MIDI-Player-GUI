58113f260be159413d9546f8169d6d61501ca4c1
user <user@localhost.localdomain>
2025-03-30 23:48:33 +0200

    added pairing switches and speed slide

--- midi.py
+++ midi.py
@@ -1,8 +1,11 @@
 import tkinter as tk
 from tkinter.filedialog import askopenfilename
 
+from threading import Thread
+
 import mido
 import re
+import time
 
 #Qsynth
 port = mido.open_output('Midi Through:Midi Through Port-0 14:0')
@@ -13,13 +16,17 @@ port = mido.open_output('Midi Through:Midi Through Port-0 14:0')
 #somehow port.panic() doesnt work on CH345
 def ownPanic():
     for ch in range(15):
-      for noteNumber in range (127):
+      for noteNumber in range(127):
+        msg = mido.Message('note_off', channel=ch, note=noteNumber)
+        port.send(msg)
+
+def ownPanic_spec(ch):
+    for noteNumber in range(127):
         msg = mido.Message('note_off', channel=ch, note=noteNumber)
         port.send(msg)
 
 def quit():
-    global running
-    running = False
+    stopplayback()
     ownPanic()
     root.destroy()
 
@@ -29,27 +36,39 @@ def stopplayback():
 
 def playmidi():
     global running
-    running = True
+    running = False
     filename = askopenfilename(filetypes = [('Midi Files', '*.mid')])
     mid = mido.MidiFile(filename)
     estplaytime = mid.length / 60
     tb.config(state='normal')
     tb.delete("1.0", "end")
     tb.tag_configure("center", justify='center')
-    tb.insert(tk.END, "Spieldauer: " + "%.2f" % estplaytime + " Minuten")
+    tb.insert(tk.END, "Spieldauer: " + "%.2f" % (estplaytime/(speed_multiplier.get()/100)) + " Minuten")
     tb.tag_add("center", "1.0", "end")
     tb.config(state='disabled')
+    running = True
 
-    for msg2 in mid.play():
-        msg2_s = str(msg2)
-        msg2_re = re.sub("channel=0", "channel=1", msg2_s)
+    for msg in mid:
+      time.sleep(msg.time * 1/((speed_multiplier.get())/100))
+      if not msg.is_meta:
+        if kop_1.get(): #II/I
+          msg_re = re.sub("channel=0", "channel=1", str(msg))
+          port.send(mido.Message.from_str(msg_re))
+        if kop_2.get(): #I/Ped
+          msg2_re = re.sub("channel=2", "channel=0", str(msg))
+          port.send(mido.Message.from_str(msg2_re))
+        if kop_3.get(): #II/Ped
+          msg3_re = re.sub("channel=2", "channel=1", str(msg))
+          port.send(mido.Message.from_str(msg3_re))
+        port.send(msg)
+
+    #for msg2 in mid.play():
+        #msg2_string = str(msg2)
+        #msg2_re = re.sub("channel=0", "channel=1", msg2_string)
         #mido.Message.from_str(msg2_re)
         #port.send(mido.Message.from_str(msg2_re))
-        msg2_reSearch = re.findall("time=", msg2_s)
-        #print(msg2_reSearch.group())
-        port.send(msg2)
-        #print(msg2_re)
-        root.update()
+
+        #root.update()
         if running == False:            
           ownPanic()
           break
@@ -143,10 +162,25 @@ def writePed2(chan, key):
 
 ##########################################################
 
+def kop1_switchOff():
+    if not kop_1.get():
+      ownPanic_spec(1) #Channel 1
+
+def kop2_switchOff():
+    if not kop_2.get():
+      ownPanic_spec(0) #Channel 1
+
+def kop3_switchOff():
+    if not kop_3.get():
+      ownPanic_spec(1) #Channel 1
+
+##########################################################
+
 if __name__ == "__main__":
     root = tk.Tk()
     root.title("MIDI-Player")
-    root.geometry("500x330")
+    root.geometry("600x330")
+    root.tk.call('tk', 'scaling',1.25)
     #root.attributes('-zoomed', True)
 
     hw_1 = tk.IntVar()
@@ -162,10 +196,17 @@ if __name__ == "__main__":
     ped_1 = tk.IntVar()
     ped_2 = tk.IntVar()
 
+    kop_1 = tk.IntVar()
+    kop_2 = tk.IntVar()
+    kop_3 = tk.IntVar()
+
+    speed_multiplier = tk.IntVar()
+    speed_multiplier.set(100)
+
     start = tk.Button(
         root,
         text='Midi-Datei auswählen...',
-        command=playmidi
+        command=lambda: Thread(target = playmidi).start()
         )
 
     tb = tk.Text(
@@ -173,6 +214,23 @@ if __name__ == "__main__":
         height = 2,
         width = 30,
         state='disabled'
+        )
+
+    speedSlider = tk.Scale(
+        root,
+        from_=70,
+        to=130,
+        orient=tk.HORIZONTAL,
+        relief=tk.FLAT,
+        length=200,
+        tickinterval=30,
+        variable=speed_multiplier
+        )
+
+    speedReset = tk.Button(
+        root,
+        text='Reset',
+        command=lambda: speed_multiplier.set(100)
         )
 
     stop = tk.Button(
@@ -189,8 +247,10 @@ if __name__ == "__main__":
 
     start.place(x=50, y=10)
     tb.place(x=20, y=50)
+    speedSlider.place(x=36, y=140)
+    speedReset.place(x=105, y=200)
     stop.place(x=110, y=100)
-    quit.place(x=110, y=210)
+    quit.place(x=110, y=280)
 
     HW_Label = tk.Label(
         root,
@@ -205,6 +265,11 @@ if __name__ == "__main__":
     PED_Label = tk.Label(
         root,
         text='Pedal'
+        )
+
+    KOP_Label = tk.Label(
+        root,
+        text='Koppeln'
         )
 
     hw_rohr = tk.Checkbutton(
@@ -273,12 +338,33 @@ if __name__ == "__main__":
 
     ped_nacht = tk.Checkbutton(
         root,
-        text='Nachhorn 4\'',
+        text='Nachthorn 4\'',
         variable=ped_2,
         command=lambda: writePed2(1,61)
         )
 
-    HW_Label.place(x=350, y=10)
+    kop_man = tk.Checkbutton(
+        root,
+        text='II/I',
+        variable=kop_1,
+        command=kop1_switchOff
+        )
+
+    kop_pedI = tk.Checkbutton(
+        root,
+        text='I/Ped',
+        variable=kop_2,
+        command=kop2_switchOff
+        )
+
+    kop_pedII = tk.Checkbutton(
+        root,
+        text='II/Ped',
+        variable=kop_3,
+        command=kop3_switchOff
+        )
+
+    HW_Label.place(x=350, y=20)
     hw_rohr.place(x=350, y=40)
     hw_prinz.place(x=350, y=60)
     hw_okt.place(x=350, y=80)
@@ -294,7 +380,12 @@ if __name__ == "__main__":
     ped_sub.place(x=350, y=280)
     ped_nacht.place(x=350, y=300)
 
+    KOP_Label.place(x=500, y=20)
+    kop_man.place(x=500, y=40)
+    kop_pedI.place(x=500, y=60)
+    kop_pedII.place(x=500, y=80)
+
     #msg = mido.Message('note_on', channel=0, note=60)
     #msg3 = mido.Message('note_off', channel=0, note=60)
 
-root.mainloop()
+    root.mainloop()
