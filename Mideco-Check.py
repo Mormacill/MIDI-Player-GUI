1a7ce8b8c2201311e42a3c8b1531b4770947f150
Mormacill <bachfreak15@googlemail.com>
2026-01-22 23:29:01 +0100

    Create Mideco-Check.py

---
+++ Mideco-Check.py
@@ -0,0 +1,43 @@
+#!/bin/python
+
+import time
+import mido
+from mido import Message
+
+#CH345 USB-Midi
+port = mido.open_output('CH345:CH345 MIDI 1 28:0')
+
+#SYSEX Messages
+
+def midTestManual():
+    for x in range(64):
+        key = x+36
+
+        print("Press key to switch note", key,"on...")
+        input()
+        msg = Message('note_on', note=key)
+        print("Switching note", key,"on\n")
+        port.send(msg)
+
+        print("Press key to switch note", key,"off...")
+        input()
+        msg = Message('note_off', note=key)
+        print("Switching note", key,"off\n")
+        port.send(msg)
+
+def midTestAutomatic():
+    for x in range(64):
+        key = x+36
+
+        msg = Message('note_on', note=key)
+        print("Switching note", key,"on\n")
+        port.send(msg)
+        time.sleep(1)
+
+        msg = Message('note_off', note=key)
+        print("Switching note", key,"off\n")
+        port.send(msg)
+        time.sleep(1)
+
+#midTestManual()
+midTestAutomatic()
